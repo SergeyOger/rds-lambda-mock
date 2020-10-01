44192c71305f3efb41c95ca428a92acc5737f274
Sergey <geniuserii85@gmail.com>
2020-10-01 23:47:11 +0300

    edited response source

--- src/main/java/com/mock/rdslambdamock/controller/DashboardController.java
+++ src/main/java/com/mock/rdslambdamock/controller/DashboardController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,12 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class DashboardController {
 
   @GetMapping("/{id}")
-  public Object getOrganizationReport(@PathVariable String id)
+  public Object getOrganizationReport(@PathVariable String id,
+      @RequestParam(required = false) Integer categoryId)
       throws IOException {
+
+    if (categoryId != null) {
+      File response = new File(
+          getClass().getClassLoader().getResource("category-example.json")
+              .getFile());
+
+      return new ObjectMapper().readValue(response, Map.class);
+    }
+
     log.info("Creating report for: {}", id);
     File response = new File(
-        getClass().getClassLoader().getResource("response.json")
+        getClass().getClassLoader().getResource("organization_report_example.json")
             .getFile());
+
     return new ObjectMapper().readValue(response, Map.class);
   }
 }
